745e2a2f673c6d03f10d0ddc1c9021851e58f9de
Memo <fretn655@gmail.com>
2026-07-13 20:34:35 -0600

    se cargan los dataframes que se utilizaran a lo largo del proyecto

---
+++ ProyectoSprint13.py
@@ -0,0 +1,14 @@
+# --------------- Importaciones
+
+import pandas as pd
+
+# --------------- Fin de las Importaciones
+
+#para tripleten
+# train = pd.read_csv("/datasets/gold_recovery_train.csv")
+# test = pd.read_csv("/datasets/gold_recovery_test.csv")
+# full = pd.read_csv("/datasets/gold_recovery_full.csv")
+
+train = pd.read_csv(r"gold_recovery_train.csv")
+test = pd.read_csv(r"/gold_recovery_test.csv")
+full = pd.read_csv(r"/gold_recovery_full.csv")
