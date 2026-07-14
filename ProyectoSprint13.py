42be01d24d95da189d1d077bf7411d6d27bb110e
Memo <fretn655@gmail.com>
2026-07-14 13:13:04 -0600

    se guardan resultado

--- ProyectoSprint13.py
+++ ProyectoSprint13.py
@@ -5,8 +5,9 @@ from matplotlib import pyplot as plt
 from sklearn.metrics import mean_absolute_error, make_scorer
 import numpy as np
 from sklearn.linear_model import LinearRegression
-from sklearn.tree import DecisionTreeRegression
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import cross_val_score
 
 # --------------- Fin de las Importaciones
 
@@ -211,7 +212,20 @@ smape_scorer = make_scorer(smape, greater_is_better=False)
 # 3.2. Entrena diferentes modelos. Evalúalos aplicando la validación cruzada. Elige el mejor modelo y pruébalo utilizando la muestra de prueba. Facilita los resultados.
 
 modelos = {
-    "regresion lineal": ,
-    "arbol de decision": ,
-    "bosque aleatorio"
+    "regresion lineal": LinearRegression(),
+    "arbol de decision": DecisionTreeRegressor(random_state=12345, max_depth=8),
+    "bosque aleatorio": RandomForestRegressor(random_state=12345, n_estimators=50, max_depth=8),
 }
+
+resultados = {}
+for nombre, modelo in modelos.items():
+    smape_rougher_cv = -cross_val_score(
+        modelo, features_train, target_train_rougher, cv=5, scoring=smape_scorer).mean()
+    smape_final_cv = -cross_val_score(modelo, features_train, target_train_final, cv=5, scoring=smape_scorer).mean()
+    smape_total_cv = 0.25 * smape_rougher_cv + 0.75 * smape_final_cv
+    resultados[nombre] = smape_total_cv
+    print(nombre)
+    print("smape rougher (cv):", smape_rougher_cv)
+    print("smape final (cv):", smape_final_cv)
+    print("smape total (cv):", smape_total_cv)
+    print()
